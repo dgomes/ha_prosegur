45f518f8e446b3843aa46ce5d9d2c25a4d6a2f56
Diogo Gomes <dgomes@ua.pt>
2023-02-28 17:19:42 +0000

    beta with multiple contracts

--- custom_components/prosegur/config_flow.py
+++ custom_components/prosegur/config_flow.py
@@ -1,15 +1,19 @@
 """Config flow for Prosegur Alarm integration."""
+from collections.abc import Mapping
 import logging
+from typing import Any, cast
 
 from pyprosegur.auth import COUNTRY, Auth
 from pyprosegur.installation import Installation
 import voluptuous as vol
 
 from homeassistant import config_entries, core, exceptions
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
-from homeassistant.helpers import aiohttp_client
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers import aiohttp_client, selector
 
-from .const import CONF_COUNTRY, DOMAIN
+from .const import CONF_COUNTRY, CONF_CONTRACT, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -24,32 +28,24 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
 
 async def validate_input(hass: core.HomeAssistant, data):
     """Validate the user input allows us to connect."""
+    session = aiohttp_client.async_get_clientsession(hass)
+    auth = Auth(session, data[CONF_USERNAME], data[CONF_PASSWORD], data[CONF_COUNTRY])
     try:
-        session = aiohttp_client.async_get_clientsession(hass)
-        auth = Auth(
-            session, data[CONF_USERNAME], data[CONF_PASSWORD], data[CONF_COUNTRY]
-        )
-        install = await Installation.retrieve(auth)
+        await Installation.list(auth)
+        return auth
     except ConnectionRefusedError:
         raise InvalidAuth from ConnectionRefusedError
     except ConnectionError:
         raise CannotConnect from ConnectionError
-
-    # Info to store in the config entry.
-    return {
-        "title": f"Contract {install.contract}",
-        "contract": install.contract,
-        "username": data[CONF_USERNAME],
-        "password": data[CONF_PASSWORD],
-        "country": data[CONF_COUNTRY],
-    }
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Prosegur Alarm."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+    entry: ConfigEntry
+    auth: Auth
+    user_input: dict
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
@@ -57,11 +53,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input:
             try:
-                info = await validate_input(self.hass, user_input)
-
-                await self.async_set_unique_id(info["contract"])
-                self._abort_if_unique_id_configured()
-
+                self.auth = await validate_input(self.hass, user_input)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except InvalidAuth:
@@ -70,16 +62,48 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 _LOGGER.exception(exception)
                 errors["base"] = "unknown"
             else:
-                user_input["contract"] = info["contract"]
-                return self.async_create_entry(title=info["title"], data=user_input)
+                self.user_input = user_input
+                return await self.async_step_choose_contract()
 
         return self.async_show_form(
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
-    async def async_step_reauth(self, data):
+    async def async_step_choose_contract(self, user_input=None):
+        """Let user decide which contract is being setup"""
+
+        if user_input:
+            await self.async_set_unique_id(user_input[CONF_CONTRACT])
+            self._abort_if_unique_id_configured()
+
+            self.user_input["contract"] = user_input[CONF_CONTRACT]
+
+            return self.async_create_entry(title=f"Contract {user_input[CONF_CONTRACT]}", data=self.user_input)
+        else:
+            try:
+                list_contracts = await Installation.list(self.auth)
+            except ConnectionRefusedError:
+                raise InvalidAuth from ConnectionRefusedError
+            except ConnectionError:
+                raise CannotConnect from ConnectionError
+
+        return self.async_show_form(
+            step_id="choose_contract",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_CONTRACT): selector.selector(
+                            {"select": {"options": [{"value": c['contractId'], "label": c['description']} for c in list_contracts]}}
+                        ),
+                }
+            )
+        )
+
+    async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
         """Handle initiation of re-authentication with Prosegur."""
-        self.entry = self.hass.config_entries.async_get_entry(self.context["entry_id"])
+        self.entry = cast(
+            ConfigEntry,
+            self.hass.config_entries.async_get_entry(self.context["entry_id"]),
+        )
         return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(self, user_input=None):
@@ -89,21 +113,20 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input:
             try:
                 user_input[CONF_COUNTRY] = self.entry.data[CONF_COUNTRY]
-                await validate_input(self.hass, user_input)
+                self.auth = await validate_input(self.hass, user_input)
 
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except InvalidAuth:
                 errors["base"] = "invalid_auth"
-            except Exception as exception:  # pylint: disable=broad-except
-                _LOGGER.exception(exception)
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
             else:
-                data = self.entry.data.copy()
                 self.hass.config_entries.async_update_entry(
                     self.entry,
                     data={
-                        **data,
+                        **self.entry.data,
                         CONF_USERNAME: user_input[CONF_USERNAME],
                         CONF_PASSWORD: user_input[CONF_PASSWORD],
                     },
